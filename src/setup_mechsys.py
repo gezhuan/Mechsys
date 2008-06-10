ef3c1b483e6def7a0310e92373fd6396c31992fb
Dorival Pedroso <dorival.pedroso@gmail.com>
2008-06-10 11:09:42 +1000

    Python module compiled sucessfully; however dependent libraries had to be set manually. TODO: write a customized CMakeLists.txt which generate the setup_mechsys.py tailored with the right dependencies

--- src/setup_mechsys.py
+++ src/setup_mechsys.py
@@ -2,9 +2,13 @@
  
 from distutils.core import setup
 from distutils.extension import Extension
+
+themodule = Extension("mechsys",
+                      ["mechsys.cpp"],
+                      include_dirs = ['../lib'],
+                      libraries    = ['lapack','boost_python'])
  
-setup(name="mechsys",
-    ext_modules=[
-        Extension("mechsys", ["mechsys.cpp"],
-        libraries = ["boost_python"])
-    ])
+setup(name        = "MechSys",
+      description = "Open library for mechanics",
+      version     =  "1.0",
+      ext_modules = [themodule])
