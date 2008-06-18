87a96814f587cb83e6834aff13944d359c99f121
Dorival Pedroso <dorival.pedroso@gmail.com>
2008-06-18 21:53:37 +1000

    ElasticRod now can be used in 2D problems

--- lib/fem/elems/elasticrod.h
+++ lib/fem/elems/elasticrod.h
@@ -33,9 +33,6 @@ class ElasticRod : public Element
 {
 
 public:
-	// Constants
-	static int NDIM;
-	
 	// Constructor
 	ElasticRod();
 
@@ -49,12 +46,12 @@ public:
 	void      SetModel        (char const * ModelName, char const * Prms, char const * Inis);
 	Element * SetNode         (int iNodeLocal, int iNodeGlobal);
 	void      UpdateState     (double TimeInc, LinAlg::Vector<double> const & dUglobal, LinAlg::Vector<double> & dFint);
-	void      BackupState     () { _N_bkp = _N; }
-	void      RestoreState    () { _N = _N_bkp; }
-	void      SetGeometryType (int Geom) { _geom = Geom; }
+	void      BackupState     () { _Sa_bkp=_Sa;  _Ea_bkp=_Ea; }
+	void      RestoreState    () { _Sa=_Sa_bkp;  _Ea=_Ea_bkp; }
+	void      SetGeometryType (int Geom) { _geom=Geom;  _n_dim=(_geom==3?3:2); }
 	void      SetProperties   (Array<double> const & EleProps) { _unit_weight=EleProps[0]; }
-	String    OutCenter       (bool PrintCaptionOnly) const { String res; res.Printf("%f",_N); return res; }
-	void      OutNodes        (LinAlg::Matrix<double> & Values, Array<String> & Labels) const;
+	String    OutCenter       (bool PrintCaptionOnly) const { String res; res.Printf("%f",_Sa); return res; }
+	void      GetLabels       (Array<String> & Labels) const;
 
 	// Derived methods (GEOMETRIC)
 	int  VTKCellType () const { return 4; } // VTK_
@@ -73,19 +70,19 @@ public:
 
 private:
 	// Data
-	double _E;                 ///< Young modulus
-	double _N;                 ///< Normal stress (compressive is positive)
-	double _N_bkp;             ///< Backup of normal stress
-	double _A;                 ///< Cross-sectional area
-	double _unit_weight;       ///< Unit weight
-	bool   _initial_state_set; ///< Was the initial state set?
+	double _E;           ///< Young modulus
+	double _Sa;          ///< Axial stress (compressive is positive)
+	double _Ea;          ///< Axial strain
+	double _Sa_bkp;      ///< Backup of axial stress
+	double _Ea_bkp;      ///< Backup axial strain
+	double _A;           ///< Cross-sectional area
+	double _unit_weight; ///< Unit weight
+	int    _n_dim;       ///< Space dimension 2D or 3D
 
 	// Private methods
 	void _calc_initial_internal_forces ();
 
 }; // class ElasticRod
-
-int ElasticRod::NDIM = 3;
 
 
 /////////////////////////////////////////////////////////////////////////////////////////// Implementation /////
@@ -94,7 +91,6 @@ int ElasticRod::NDIM = 3;
 /* public */
 	
 inline ElasticRod::ElasticRod()
-	: _initial_state_set(false)
 {
 	// Setup nodes number
 	_n_nodes        = 2;
@@ -113,13 +109,14 @@ inline ElasticRod::ElasticRod()
 
 inline bool ElasticRod::IsReady() const
 {
-	if (_connects.Size()==static_cast<size_t>(_n_nodes)) return true;
+	if (_connects.Size()==static_cast<size_t>(_n_nodes) && _geom>0) return true;
 	else return false;
 }
 
 inline bool ElasticRod::IsEssential(char const * DOFName) const
 {
-	if (strncmp(DOFName,"ux",2)==0 || strncmp(DOFName,"uy",2)==0 || strncmp(DOFName,"uz",2)==0) return true;
+	if (strcmp(DOFName,"ux")==0 || strcmp(DOFName,"uy")==0) return true;
+	if (_n_dim==3 && strcmp(DOFName,"uz")==0)               return true;
 	else return false;
 }
 
@@ -135,9 +132,9 @@ inline void ElasticRod::SetModel(char const * ModelName, char const * Prms, char
 	int count = 0;
 	if (names.Size()==1 && values.Size()==1)
 		if (names[0]=="E") { _E = values[0];  count++; }
-	if (count!=1) throw Fatal("ElasticRod::SetModel: Parameters definition is incorrect. The syntax must be as in:\n\t E=10000.0\n");
+	if (count!=1) throw new Fatal("ElasticRod::SetModel: Parameters definition is incorrect. The syntax must be as in:\n\t E=10000.0\n");
 
-	/* Inis   "N=0.0  A=1.0" */
+	/* Inis   "Sa=0.0  A=1.0" */
 	lp.Reset(Inis);
 	lp.BreakExpressions(names,values);
 
@@ -147,14 +144,14 @@ inline void ElasticRod::SetModel(char const * ModelName, char const * Prms, char
 	{
 		for (size_t i=0; i<names.Size(); ++i)
 		{
-				 if (names[i]=="N") { _N = values[i];  count++; }
-			else if (names[i]=="A") { _A = values[i];  count++; }
+				 if (names[i]=="Sa") { _Sa = values[i];  count++; } // axial stress
+			else if (names[i]=="A")  { _A  = values[i];  count++; } // area
 		}
 	}
-	if (count!=2) throw Fatal("LinElastic::SetInis: Initial values definition is incorrect. The syntax must be as in:\n\t N=0.0  A=1.0\n");
+	if (count!=2) throw new Fatal("LinElastic::SetInis: Initial values definition is incorrect. The syntax must be as in:\n\t Sa=0.0  A=1.0\n");
 
 	// If the initial state was not set, compute initial internal forces
-	if (!_initial_state_set) _calc_initial_internal_forces();
+	_calc_initial_internal_forces();
 }
 
 inline Element * ElasticRod::SetNode(int iNodeLocal, int iNodeGlobal)
@@ -165,6 +162,7 @@ inline Element * ElasticRod::SetNode(int iNodeLocal, int iNodeGlobal)
 	// Add Degree of Freedom to a node (Essential, Natural)
 	Nodes[iNodeGlobal]->AddDOF("ux", "fx");
 	Nodes[iNodeGlobal]->AddDOF("uy", "fy");
+	if (_n_dim==3)
 	Nodes[iNodeGlobal]->AddDOF("uz", "fz");
 
 	// Shared
@@ -176,57 +174,86 @@ inline Element * ElasticRod::SetNode(int iNodeLocal, int iNodeGlobal)
 inline void ElasticRod::UpdateState(double TimeInc, LinAlg::Vector<double> const & dUglobal, LinAlg::Vector<double> & dFint)
 {
 	// Allocate (local/element) displacements vector
-	LinAlg::Vector<double> dU(NDIM*_n_nodes); // Delta disp. of this element
+	LinAlg::Vector<double> dU(_n_dim*_n_nodes); // Delta disp. of this element
 	
 	// Assemble (local/element) displacements vector
 	for (int i=0; i<_n_nodes; ++i)
 	{
-		dU(i*NDIM  ) = dUglobal(_connects[i]->DOFVar("ux").EqID);
-		dU(i*NDIM+1) = dUglobal(_connects[i]->DOFVar("uy").EqID);
-		dU(i*NDIM+2) = dUglobal(_connects[i]->DOFVar("uz").EqID);
+		dU(i*_n_dim  ) = dUglobal(_connects[i]->DOFVar("ux").EqID);
+		dU(i*_n_dim+1) = dUglobal(_connects[i]->DOFVar("uy").EqID);
+		if (_n_dim==3)
+		dU(i*_n_dim+2) = dUglobal(_connects[i]->DOFVar("uz").EqID);
 	}
 	
 	// Allocate (local/element) internal force vector
-	LinAlg::Vector<double> dF(NDIM*_n_nodes); // Delta internal force of this element
+	LinAlg::Vector<double> dF(_n_dim*_n_nodes); // Delta internal force of this element
 	dF.SetValues(0.0);
 
 	// Compute dF
 	LinAlg::Matrix<double> Ke;
 	Order1Matrix(0,Ke);
 	LinAlg::Gemv(1.0,Ke,dU, 0.0,dF); // dF <- Ke*dU
-	_N += dF(1)-dF(0);
+	_Sa += dF(1)-dF(0);
 
 	// Return internal forces
 	for (int i=0; i<_n_nodes; ++i)
 	{
 		// Sum up contribution to internal forces vector
-		dFint(_connects[i]->DOFVar("fx").EqID) += dF(i*NDIM  );
-		dFint(_connects[i]->DOFVar("fy").EqID) += dF(i*NDIM+1);
-		dFint(_connects[i]->DOFVar("fz").EqID) += dF(i*NDIM+2);
+		dFint(_connects[i]->DOFVar("fx").EqID) += dF(i*_n_dim  );
+		dFint(_connects[i]->DOFVar("fy").EqID) += dF(i*_n_dim+1);
+		if (_n_dim==3)
+		dFint(_connects[i]->DOFVar("fz").EqID) += dF(i*_n_dim+2);
 	}
 }
 
-inline void ElasticRod::OutNodes(LinAlg::Matrix<double> & Values, Array<String> & Labels) const
+inline void ElasticRod::GetLabels(Array<String> & Labels) const
 {
-	int const DATA_COMPS=6;
-	Values.Resize(_n_nodes,DATA_COMPS);
-	Labels.Resize(DATA_COMPS);
-	Labels[ 0] = "ux"; Labels[ 1] = "uy"; Labels[ 2] = "uz"; 
-	Labels[ 3] = "fx"; Labels[ 4] = "fy"; Labels[ 5] = "fz";
-	for (int i_node=0; i_node<_n_nodes; i_node++)
+	// Get labels of all values to output
+	switch (_geom) // 1:1D, 2:2D(plane-strain), 3:3D, 4:2D(axis-symmetric), 5:2D(plane-stress)
 	{
-		Values(i_node,0) = _connects[i_node]->DOFVar("ux").EssentialVal;
-		Values(i_node,1) = _connects[i_node]->DOFVar("uy").EssentialVal;
-		Values(i_node,2) = _connects[i_node]->DOFVar("uz").EssentialVal;
-		Values(i_node,3) = _connects[i_node]->DOFVar("fx").NaturalVal;
-		Values(i_node,4) = _connects[i_node]->DOFVar("fy").NaturalVal;
-		Values(i_node,5) = _connects[i_node]->DOFVar("fz").NaturalVal;
+		case 2: // 2D(plane-strain)
+		case 5: // 2D(plane-stress)
+		{
+			Labels.Resize(6); // 6 values to output
+			Labels[0]="ux"; Labels[1]="uy"; // Displacements
+			Labels[2]="fx"; Labels[3]="fy"; // Forces
+			Labels[4]="Ea";                 // Strains (axial strain)
+			Labels[5]="Sa";                 // Stress (axial stress)
+			return;
+		}
+		case 3: // 3D
+		{
+			Labels.Resize(8); // 8 values to output
+			Labels[0]="ux"; Labels[1]="uy"; Labels[2]="uz"; // Displacements
+			Labels[3]="fx"; Labels[4]="fy"; Labels[5]="fz"; // Forces
+			Labels[6]="Ea";                                 // Strains (axial strain)
+			Labels[7]="Sa";                                 // Stress (axial stress)
+			return;
+		}
+		case 1: // 1D
+		case 4: // 2D(axis-symmetric)
+		default:
+			throw new Fatal("EquilibElem::GetLabels: GeometryType==%d is not implemented yet",_geom);
 	}
 }
 
 inline double ElasticRod::Val(int iNodeLocal, char const * Name) const
 {
-	throw new Fatal("ElasticRod::Val: Feature not implemented yet",Name);
+	// Displacements
+	if (strcmp(Name,"ux")==0 || strcmp(Name,"uy")==0 || strcmp(Name,"uz")==0)
+		return _connects[iNodeLocal]->DOFVar(Name).EssentialVal;
+
+	// Forces
+	else if (strcmp(Name,"fx")==0 || strcmp(Name,"fy")==0 || strcmp(Name,"fz")==0)
+		return _connects[iNodeLocal]->DOFVar(Name).NaturalVal;
+
+	// Stress, strains, internal values, etc.
+	else
+	{
+		     if (strcmp(Name,"Sa")==0) return _Sa;
+		else if (strcmp(Name,"Ea")==0) return _Ea;
+		else throw new Fatal("ElasticRod::Val: value named %s is not available for this element",Name);
+	}
 }
 
 // Derived methods to assemble DAS matrices
@@ -234,7 +261,7 @@ inline double ElasticRod::Val(int iNodeLocal, char const * Name) const
 inline void ElasticRod::Order1MatMap(size_t Index, Array<size_t> & RowsMap, Array<size_t> & ColsMap, Array<bool> & RowsEssenPresc, Array<bool> & ColsEssenPresc) const
 {
 	// Size of Ke
-	int n_rows = NDIM*_n_nodes; // == n_cols
+	int n_rows = _n_dim*_n_nodes; // == n_cols
 
 	// Mounting a map of positions from Ke to Global
 	int idx_Ke = 0;                // position (idx) inside Ke matrix
@@ -250,9 +277,12 @@ inline void ElasticRod::Order1MatMap(size_t Index, Array<size_t> & RowsMap, Arra
 		RowsMap        [idx_Ke] = _connects[i_node]->DOFVar("uy").EqID; 
 		RowsEssenPresc [idx_Ke] = _connects[i_node]->DOFVar("uy").IsEssenPresc; 
 		idx_Ke++;
-		RowsMap        [idx_Ke] = _connects[i_node]->DOFVar("uz").EqID; 
-		RowsEssenPresc [idx_Ke] = _connects[i_node]->DOFVar("uz").IsEssenPresc; 
-		idx_Ke++;
+		if (_n_dim==3)
+		{
+			RowsMap        [idx_Ke] = _connects[i_node]->DOFVar("uz").EqID; 
+			RowsEssenPresc [idx_Ke] = _connects[i_node]->DOFVar("uz").IsEssenPresc; 
+			idx_Ke++;
+		}
 	}
 	ColsMap        = RowsMap;
 	ColsEssenPresc = RowsEssenPresc;
@@ -261,20 +291,34 @@ inline void ElasticRod::Order1MatMap(size_t Index, Array<size_t> & RowsMap, Arra
 inline void ElasticRod::Order1Matrix(size_t index, LinAlg::Matrix<double> & Ke) const
 {
 	// Resize Ke
-	Ke.Resize(NDIM*_n_nodes, NDIM*_n_nodes); // sum(Bt*D*B*det(J)*w)
+	Ke.Resize(_n_dim*_n_nodes, _n_dim*_n_nodes); // sum(Bt*D*B*det(J)*w)
 	Ke.SetValues(0.0);
 
-	double x21 = _connects[1]->X()-_connects[0]->X();
-	double y21 = _connects[1]->Y()-_connects[0]->Y();
-	double z21 = _connects[1]->Z()-_connects[0]->Z();
-	double L   = sqrt(x21*x21+y21*y21+z21*z21);
-	       Ke  =  x21*x21, x21*y21, x21*z21,-x21*x21,-x21*y21,-x21*z21,
-                  y21*x21, y21*y21, y21*z21,-y21*x21,-y21*y21,-y21*z21,
-                  z21*x21, z21*y21, z21*z21,-z21*x21,-z21*y21,-z21*z21,
-                 -x21*x21,-x21*y21,-x21*z21, x21*x21, x21*y21, x21*z21,
-                 -y21*x21,-y21*y21,-y21*z21, y21*x21, y21*y21, y21*z21,
-                 -z21*x21,-z21*y21,-z21*z21, z21*x21, z21*y21, z21*z21;
-	       Ke  = Ke * (_E*_A/pow(L,3.0));
+	double L;
+	if (_n_dim==2) // 2D
+	{
+		double x21 = _connects[1]->X()-_connects[0]->X();
+		double y21 = _connects[1]->Y()-_connects[0]->Y();
+		       L   = sqrt(x21*x21+y21*y21);
+		       Ke  =  x21*x21, x21*y21,-x21*x21,-x21*y21,
+		              y21*x21, y21*y21,-y21*x21,-y21*y21,
+		             -x21*x21,-x21*y21, x21*x21, x21*y21,
+		             -y21*x21,-y21*y21, y21*x21, y21*y21;
+	}
+	else // 3D
+	{
+		double x21 = _connects[1]->X()-_connects[0]->X();
+		double y21 = _connects[1]->Y()-_connects[0]->Y();
+		double z21 = _connects[1]->Z()-_connects[0]->Z();
+		       L   = sqrt(x21*x21+y21*y21+z21*z21);
+		       Ke  =  x21*x21, x21*y21, x21*z21,-x21*x21,-x21*y21,-x21*z21,
+		              y21*x21, y21*y21, y21*z21,-y21*x21,-y21*y21,-y21*z21,
+		              z21*x21, z21*y21, z21*z21,-z21*x21,-z21*y21,-z21*z21,
+		             -x21*x21,-x21*y21,-x21*z21, x21*x21, x21*y21, x21*z21,
+		             -y21*x21,-y21*y21,-y21*z21, y21*x21, y21*y21, y21*z21,
+		             -z21*x21,-z21*y21,-z21*z21, z21*x21, z21*y21, z21*z21;
+	}
+	Ke = Ke * (_E*_A/pow(L,3.0));
 }
 
 /* private */
