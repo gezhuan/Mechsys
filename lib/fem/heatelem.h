833c00de8d216c685e22c4a3398b933a00f991c3
Raul Durand <raul.durand@gmail.com>
2008-06-24 16:00:31 +1000

    some comments added

--- lib/fem/heatelem.h
+++ lib/fem/heatelem.h
@@ -19,8 +19,8 @@
 
 /* __ Equilibrium capable element __
 
-T = Nodal temperature increment
-F = Nodal heat flow
+T = Nodal temperature [ temp ]     ex. C°
+F = Nodal heat flow   [ work/time] ex. Joules/sec
 
 */
 
@@ -94,8 +94,7 @@ inline bool HeatElem::IsReady() const
 
 inline bool HeatElem::IsEssential(char const * DOFName) const
 {
-	if (strcmp(DOFName,"T")==0) return true;
-
+	if (strcmp(DOFName,"T")==0) return true; // Temperature is the essential variable
 	return false;
 }
 
@@ -112,7 +111,6 @@ inline void HeatElem::SetModel(char const * ModelName, char const * Prms, char c
 		{
 			// Allocate a new model and set parameters
 			_a_model[i] = static_cast<HeatModel*>(AllocModel(ModelName));
-			//_a_model[i]->SetGeom(_geom);
 			_a_model[i]->SetPrms(Prms);
 			_a_model[i]->SetInis(Inis);
 		}
@@ -129,7 +127,7 @@ inline Element * HeatElem::SetNode(int iNodeLocal, int iNodeGlobal)
 	_connects[iNodeLocal] = Nodes[iNodeGlobal];
 
 	// Add Degree of Freedom to a node (Essential, Natural)
-	Nodes[iNodeGlobal]->AddDOF("T", "F");
+	Nodes[iNodeGlobal]->AddDOF("T", "F"); // T stands for temperature and F for nodal heat source
 
 	// Shared
 	Nodes[iNodeGlobal]->SetSharedBy(_my_id);
@@ -137,25 +135,25 @@ inline Element * HeatElem::SetNode(int iNodeLocal, int iNodeGlobal)
 	return this;
 }
 
-inline void HeatElem::UpdateState(double TimeInc, LinAlg::Vector<double> const & dUglobal, LinAlg::Vector<double> & dQint)
+inline void HeatElem::UpdateState(double TimeInc, LinAlg::Vector<double> const & dUglobal, LinAlg::Vector<double> & dFint)
 {
 	// Allocate (local/element) displacements vector
-	LinAlg::Vector<double> dT(_n_nodes); // Delta disp. of this element
+	LinAlg::Vector<double> dT(_n_nodes); // Delta temp. of this element
 	
 	// Assemble (local/element) temperatures vector
 	for (size_t i=0; i<_n_nodes; ++i)
 	   dT(i) = dUglobal(_connects[i]->DOFVar("T").EqID);
 	
 	// Allocate (local/element) internal force vector
-	LinAlg::Vector<double> dQ(_n_nodes); // Delta internal force of this element
-	dQ.SetValues(0.0);
+	LinAlg::Vector<double> dF(_n_nodes); // Delta internal heat source of this element
+	dF.SetValues(0.0);
 	
 	// Allocate entities used for every integration point
 	LinAlg::Matrix<double> derivs;  // size = NumLocalCoords(ex.: r,s,t) x _n_nodes
 	LinAlg::Matrix<double> J;       // Jacobian matrix
 	LinAlg::Matrix<double> B;       // strain-displacement matrix
-	LinAlg::Vector<double> DGrad;    // Gradient vector 
-	LinAlg::Vector<double> DFlow;    // Stress vector 
+	LinAlg::Vector<double> DGrad;   // Gradient vector 
+	LinAlg::Vector<double> DFlow;   // Stress vector 
 
 	// Loop along integration points
 	for (size_t i_ip=0; i_ip<_n_int_pts; ++i_ip)
@@ -170,21 +168,21 @@ inline void HeatElem::UpdateState(double TimeInc, LinAlg::Vector<double> const &
 		Jacobian (derivs, J);      // Calculate J (Jacobian) matrix for i_ip Integration Point
 		B_Matrix (derivs, J, B);   // Calculate B matrix for i_ip Integration Point
 
-		// Calculate a tensor for the increments of strain
+		// Calculate the gradient
 		DGrad = B*dT;
 		
 		// Update model
 		_a_model[i_ip]->UpdateState(DGrad, DFlow);
 
 		// Calculate internal force vector;
-		dQ += trn(B)*DFlow*det(J)*w;
+		dF += trn(B)*DFlow*det(J)*w;
 	}
 
-	// Return internal forces
+	// Return internal F
 	for (size_t i=0; i<_n_nodes; ++i)
 	{
 		// Sum up contribution to internal forces vector
-		dQint(_connects[i]->DOFVar("F").EqID) += dQ(i);
+		dFint(_connects[i]->DOFVar("F").EqID) += dF(i);
 	}
 }
 
@@ -204,8 +202,8 @@ inline void HeatElem::GetLabels(Array<String> & Labels) const
 {
 	// Get labels of all values to output
 	Labels.Resize(2);
-	Labels[0]="T"; // Temperature
-	Labels[1]="F"; // Heat flow
+	Labels[0]="T"; // Nodal temperature
+	Labels[1]="F"; // Nodal heat source
 }
 
 inline double HeatElem::Val(int iNodeLocal, char const * Name) const
@@ -234,7 +232,7 @@ inline double HeatElem::Val(char const * Name) const
 
 inline void HeatElem::Deactivate()
 {
-	throw new Fatal("HeatElem::Deactivate: Feature not implemented yet");
+	throw new Fatal("HeatElem::Deactivate: Feature not implemented in this element");
 }
 
 // Derived methods to assemble DAS matrices
@@ -312,7 +310,7 @@ inline void HeatElem::B_Matrix(LinAlg::Matrix<double> const & derivs, LinAlg::Ma
 
 /* private */
 
-inline void HeatElem::_calc_initial_internal_forces()
+inline void HeatElem::_calc_initial_internal_forces() // TODO
 {
 	// Allocate (local/element) internal force vector
 	LinAlg::Vector<double> F(_n_nodes);
