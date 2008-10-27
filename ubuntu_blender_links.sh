e48e0feb64cc22a46a3307dc4f03958138e65cbe
Dorival Pedroso <dorival.pedroso@gmail.com>
2008-10-27 20:41:04 +1000

    Blender Module: script names changed

--- ubuntu_blender_links.sh
+++ ubuntu_blender_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MODULES="3dlink cad dict draw fem shandler"
+MODULES="3dlink cad dict fem main mesh shandler"
 for m in $MODULES; do
 	ln -s $HOME/mechsys/lib/blender/msys_$m.py $HOME/.blender/scripts
 done
