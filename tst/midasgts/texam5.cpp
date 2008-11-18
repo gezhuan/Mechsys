f8c68bdf7279ec4e7183bc6703eb7e23a80e36b4
Raul Durand <raul.durand@gmail.com>
2008-11-18 21:20:23 +1000

    Test midas texam5 improved. Not working yet

--- tst/midasgts/texam5.cpp
+++ tst/midasgts/texam5.cpp
@@ -40,6 +40,41 @@ using Util::_8s;
 using Util::PI;
 using boost::make_tuple;
 
+void Einsten_Schwartz(
+		double p0,   // initial stress load
+		double K,    // ratio beetween horizontal and vertical initial stress load
+		double R,    // radius: polar coordinate
+		double th,   // angle:  polar coordinate
+		double r,    // radius of the hole
+		double E,    // rock Young modulus
+		double nu,   // rock Poisson ratio
+		double E_s,  // liner Young modulus
+		double nu_s, // liner Poisson ratio
+		double I,    // liner moment of inertia
+		double A,    // cross sectional area of the liner for a 1m long section
+		double & M,  // bending moment in the liner
+		double & N,  // axial force in the liner
+		double & uT, // tangential displacement
+		double & uR  // radial displacement
+		)
+{
+	double Nu   = 1.0-nu;
+	double Nu2  = 1.0-nu*nu;
+	double Nus2 = 1.0-nu_s*nu_s;
+	double C    = E*R*Nus2 / (E_s*A*Nu2);
+	double F    = E*pow(R,3)*Nus2 / ( E_s*I*(1.0-nu*nu) );
+	double beta = ((6.0+F)*C*Nu + 2*F*nu) / (3*F + 3*C + 2*C*F*Nu);
+	double b2   = C*Nu * 0.5 / (C*Nu + 4*nu - 6*beta - 3*beta*C*Nu);
+	double a2   = beta*b2;
+	double a0   = C*F*Nu / (C+F+C*F*Nu);
+	double G    = E/2.0/(1+nu);
+
+	M = p0*r*r*0.25*(1.0-K)*(1.0 - 2.0*a2 + 2.0*b2)*cos(2.0*th);
+	N = p0*r*(0.5*(1.0+K)*(1.0 - a0) + 0.5*(1.0-K)*(1.0+2*a2)*cos(2.0*th) );
+	uT = 0.5*p0*r/G*(1.0-K)*(a2 + (1.0 - 2*nu)*b2)*sin(2.0*th);
+	uR = 0.5*p0*r/G*(-0.5*(1.0+K)*a0 + 0.5*(1.0-K)*( 4.0*(1.0-nu)*b2 - 2.0*a2 )*cos(2*th) );
+}
+
 int main(int argc, char **argv) try
 {
 	// Constants
@@ -133,10 +168,12 @@ int main(int argc, char **argv) try
 
 	// Edges brys
 	FEM::EBrys_T ebrys;
-	ebrys.Push (make_tuple(-10, "fx", -30.0));
-	ebrys.Push (make_tuple(-20, "uy",   0.0));
-	ebrys.Push (make_tuple(-30, "fy", -15.0));
-	ebrys.Push (make_tuple(-40, "ux",   0.0));
+	double p0 = -15.0;
+	double  K =  2.0;
+	ebrys.Push (make_tuple(-10, "fx", p0*K));
+	ebrys.Push (make_tuple(-20, "uy",  0.0));
+	ebrys.Push (make_tuple(-30, "fy",   p0));
+	ebrys.Push (make_tuple(-40, "ux",  0.0));
 
 	// Elements attributes
 	String prms; prms.Printf("E=%f nu=%f",E_soil,nu_soil);
@@ -164,6 +201,77 @@ int main(int argc, char **argv) try
 	cout << "Time elapsed (solution) = "<<static_cast<double>(total)/CLOCKS_PER_SEC<<" seconds\n";
 	cout << "[1;35mNorm(Resid=DFext-DFint) = " << norm_resid << "[0m\n";
 	cout << "[1;32mNumber of DOFs          = " << sol->nDOF() << "[0m\n";
+
+	//////////////////////////////////////////////////////////////////////////////////////// Check /////
+
+    Vector<double> sig(3);
+    Vector<double> sig_k(3);
+    Array <double> err_sig_x;
+    Array <double> err_sig_y;
+    Array <double> err_sig_xy;
+    Array <double> err_disp;
+
+	// Stress 
+	//for (size_t i=0; i<g.NElems(); ++i)
+	//{
+	//	for (size_t j=0; j<g.Ele(i)->NNodes(); ++j)
+	//	{
+	//		// Analytical
+	//		double x  = g.Ele(i)->Nod(j)->X();
+	//		double y  = g.Ele(i)->Nod(j)->Y();
+	//		double th = atan(y/x);
+	//		double R  = sqrt(x*x + y*y);
+	//		//Kirsch_stress(p1, p2, r, R, th, sig_k); // Calculate the Kirsch solution for a cylindrical hole
+
+
+
+	//		// Analysis
+	//		sig = g.Ele(i)->Val(j,"Sx"), g.Ele(i)->Val(j,"Sy"), g.Ele(i)->Val(j,"Sxy");
+	//		Vector<double> sig_polar(3); // Vector to transform to polar coordinates
+	//		sig_polar = sig(0)*pow(cos(th),2.0) + sig(1)*pow(sin(th),2.0) + 2.0*sig(2)*sin(th)*cos(th),
+	//		            sig(0)*pow(sin(th),2.0) + sig(1)*pow(cos(th),2.0) - 2.0*sig(2)*sin(th)*cos(th),
+	//		            (sig(1)-sig(0))*sin(th)*cos(th) + sig(2)*(pow(cos(th),2.0) - pow(sin(th),2.0));
+
+	//		err_sig_x .Push ( fabs(sig_k(0) - sig_polar(0)) );
+	//		err_sig_y .Push ( fabs(sig_k(1) - sig_polar(1)) );
+	//		err_sig_xy.Push ( fabs(sig_k(2) - sig_polar(2)) );
+	//	}
+	//}
+
+	// Displacements
+	for (size_t i=0; i<g.NNodes(); ++i)
+	{
+		// Analytical
+		double x  = g.Nod(i)->X();
+		double y  = g.Nod(i)->Y();
+		double th = atan(y/x);
+		double R  = sqrt(x*x + y*y);
+		double uR_correct;
+		double uT_correct;
+		double nu_beam = 0.25;
+		double M, N;
+
+		Einsten_Schwartz( p0, K, R, th, r, E_soil, nu_soil, E_beam, nu_beam, Izz_beam, A_beam, M, N, uT_correct, uR_correct);
+
+		// Analysis
+		double ux = g.Nod(i)->Val("ux");
+		double uy = g.Nod(i)->Val("uy");
+		double uR = ux*cos(th) + uy*sin(th);
+		double uT = uy*cos(th) - ux*sin(th);
+
+		err_disp.Push ( uR - uR_correct) ;
+		err_disp.Push ( uT - uT_correct) ;
+	}
+	// Error summary
+	double tol_disp       = 1.0e-1;
+	double min_err_disp   = err_disp  [err_disp.Min()];
+	double max_err_disp   = err_disp  [err_disp.Max()];
+	cout << _4 << ""      << _8s <<"Min"       << _8s << "Mean"                                                        << _8s<<"Max"                  << _8s<<"Norm"         << endl;
+	cout << _4 << "Disp"  << _8s <<min_err_disp << _8s<<err_disp  .Mean() << (max_err_disp  >tol_disp?"[1;31m":"[1;32m") << _8s<<max_err_disp   << "[0m" << _8s<<err_disp  .Norm() << endl;
+	cout << endl;
+
+
+
 
 	// Output: VTU
 	Output o; o.VTU (&g, "texam5.vtu");
