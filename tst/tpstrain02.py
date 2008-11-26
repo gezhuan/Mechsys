1a1f915a25c047f1dff26f7ef73643398625f389
Dorival Pedroso <dorival.pedroso@gmail.com>
2008-11-26 13:42:51 +1000

    tpstrain02 updated according to new sign convention (Solid Mechanics:Traction=positive)

--- tst/tpstrain02.py
+++ tst/tpstrain02.py
@@ -23,9 +23,9 @@ L  = 2.0   # Length
 H  = 2.0   # Height
 E  = 207.0 # Young
 nu = 0.3   # Poisson
-q  = 1.0   # Load
-nx = 20    # Divisions along x (must be even)
-ny = 20    # Divisions along y
+q  = -1.0  # Load
+nx = 10    # Divisions along x (must be even)
+ny = 10    # Divisions along y
 
 # ----------------------------------------------------------------------------- Mesh
 
@@ -66,7 +66,7 @@ nbrys = [[L/2., 0.0, 0.0, 'ux', 0.0]] # x,y,z, key, val
 
 # Edges brys
 ebrys = [[-10, 'uy', 0.0], # [tag], [key], [val]
-         [-20, 'fy',  -q]] # [tag], [key], [val]
+         [-20, 'fy',   q]] # [tag], [key], [val]
 
 # Elements attributes
 eatts = [[-1, 'Quad4PStrain', 'LinElastic', 'E=%f nu=%f'%(E,nu), 'Sx=0.0 Sy=0.0 Sz=0.0 Sxy=0.0']] # [tag], [type], [model], [prms], [inis]
@@ -87,48 +87,49 @@ print 'File <tpstrain02_py.vtu> generated'
 
 #----------------------------------------------------------------------------- Check
 
-# Check
-errors = 0.0
+err_eps = []
+err_sig = []
+err_dis = []
 
-Sy = q
-Ex = -nu*(1.0+nu)*Sy/E
-Ey =  (1.0-nu*nu)*Sy/E
-Sz = (E/(1.0+nu))*(nu/(1.0-2.0*nu))*(Ex+Ey)
+Sx  = 0.0
+Sy  = q
+Ex  = -nu*(1.0+nu)*Sy/E
+Ey  =  (1.0-nu*nu)*Sy/E
+Ez  = 0.0
+Exy = 0.0
+Sz  = (E/(1.0+nu))*(nu/(1.0-2.0*nu))*(Ex+Ey)
+Sxy = 0.0
 
 # Stress and strains
 for i in range(g.nelems()):
-    errors += abs(g.ele(i).val("Ex" ) - (Ex))
-    errors += abs(g.ele(i).val("Ey" ) - (Ey))
-    errors += abs(g.ele(i).val("Exy") - (0.0))
-    errors += abs(g.ele(i).val("Sx" ) - (0.0))
-    errors += abs(g.ele(i).val("Sy" ) - (Sy ))
-    errors += abs(g.ele(i).val("Sz" ) - (Sz ))
-    errors += abs(g.ele(i).val("Sxy") - (0.0))
+    for j in range(g.ele(i).nnodes()):
+        err_eps.append ( abs(g.ele(i).val(j,"Ex" ) - Ex ) / (1.0+abs(Ex )) )
+        err_eps.append ( abs(g.ele(i).val(j,"Ey" ) - Ey ) / (1.0+abs(Ey )) )
+        err_eps.append ( abs(g.ele(i).val(j,"Ez" ) - Ez ) / (1.0+abs(Ez )) )
+        err_eps.append ( abs(g.ele(i).val(j,"Exy") - Exy) / (1.0+abs(Exy)) )
+        err_sig.append ( abs(g.ele(i).val(j,"Sx" ) - Sx ) / (1.0+abs(Sx )) )
+        err_sig.append ( abs(g.ele(i).val(j,"Sy" ) - Sy ) / (1.0+abs(Sy )) )
+        err_sig.append ( abs(g.ele(i).val(j,"Sz" ) - Sz ) / (1.0+abs(Sz )) )
+        err_sig.append ( abs(g.ele(i).val(j,"Sxy") - Sxy) / (1.0+abs(Sxy)) )
 
 # Displacements
 for i in range(g.nnodes()):
-    # bottom nodes
-    if (abs(g.nod(i).y())<1.0e-5):
-        errors += abs(g.nod(i).val("uy") - (0.0))
+    ux_correct = Ex*(g.nod(i).x()-L/2.)
+    uy_correct = Ey* g.nod(i).y()
+    err_dis.append ( abs(g.nod(i).val("ux") - ux_correct) / (1.0+abs(ux_correct)) )
+    err_dis.append ( abs(g.nod(i).val("uy") - uy_correct) / (1.0+abs(uy_correct)) )
 
-    # fixed bottom (central) node
-    if (abs(g.nod(i).y())<1.0e-5) and (abs(g.nod(i).x()-(L/2.0))<1.0e-5):
-        errors += abs(g.nod(i).val("ux") - (0.0))
-
-    # half-height nodes
-    if (abs(g.nod(i).y()-(H/2.0))<1.0e-5):
-        errors += abs(g.nod(i).val("uy") - (-0.5*H*Ey))
-
-    # top nodes
-    if (abs(g.nod(i).y()-(H))<1.0e-5):
-        errors += abs(g.nod(i).val("uy") - (-H*Ey))
-
-    # left nodes
-    if (abs(g.nod(i).x())<1.0e-5):
-        errors += abs(g.nod(i).val("ux") - (0.5*L*Ex))
-
-    # right nodes
-    if (abs(g.nod(i).x()-L)<1.0e-5):
-        errors += abs(g.nod(i).val("ux") - (-0.5*L*Ex))
-
-print 'Py:Errors = ', errors
+# Error summary
+min_err_eps = min(err_eps)
+min_err_sig = min(err_sig)
+min_err_dis = min(err_dis)
+max_err_eps = max(err_eps)
+max_err_sig = max(err_sig)
+max_err_dis = max(err_dis)
+print
+print 'Errors: -----------------------------------------------------------------------'
+print "      Min            Max"
+print "Eps   %8e   %8e" % (min_err_eps,max_err_eps)
+print "Sig   %8e   %8e" % (min_err_sig,max_err_sig)
+print "Dis   %8e   %8e" % (min_err_dis,max_err_dis)
+print
