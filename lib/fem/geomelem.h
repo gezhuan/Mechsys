411b975013f31b2893701ca908ff5dce07814d91
Dorival Pedroso <dorival.pedroso@gmail.com>
2008-12-11 10:19:25 +1000

    GeomElem improved. NOT working yet

--- lib/fem/geomelem.h
+++ lib/fem/geomelem.h
@@ -62,6 +62,12 @@ public:
 	virtual void         GetFNodes (int FaceID, Array<Node*> & FConn) const =0;
 	virtual double       BoundDist (double r, double s, double t) const     =0;
 
+	// Methods
+	        void Jacobian  (Mat_t const & dN, Mat_t & J) const;
+	virtual void Shape     (double r, double s, double t, Vec_t & N)  const =0;
+	virtual void FaceShape (double r, double s, Vec_t & N)            const =0;
+	virtual void Derivs    (double r, double s, double t, Mat_t & dN) const =0;
+
 	// Public data (read only)
 	size_t             NDim;       ///< Space dimension (2D or 3D)
 	size_t             NNodes;     ///< Number of nodes
@@ -70,18 +76,11 @@ public:
 	size_t             NFaceIPs;   ///< Number of integration points of face
 	IntegPoint const * IPs;        ///< Integration points
 	IntegPoint const * FaceIPs;    ///< Integration points of Faces/Edges
-	Array<Node*>       Conn;       ///< Connectivity (pointers to nodes of this element) Size==NNodes
+	Array<Node*>       Conn;       ///< Connectivity (size==NNodes). Initialized by ProbElem
 
 private:
-	// To be derived
-	virtual void _initialize () =0;
-	virtual void _shape      (double r, double s, double t, Vec_t & N)  const =0;
-	virtual void _face_shape (double r, double s, Vec_t & N)            const =0;
-	virtual void _derivs     (double r, double s, double t, Mat_t & dN) const =0;
-
-	// Private methods
-	void _jacobian (Mat_t const & dN, Mat_t & J) const;
-	//_local_coords
+	virtual void _initialize   ()          =0; ///< Initialize NNodes, NFaceNodes, NIPs, NFaceIPs, IPs, and FaceIPs
+	virtual void _local_coords (Mat_t & C) =0; ///< Return the local coordinates of the nodes
 
 }; // class GeomElem
 
@@ -105,8 +104,8 @@ inline double GeomElem::Volume() const
 	double vol=0.0;
 	for (size_t i=0; i<NIPs; ++i)
 	{
-		Derivs    (IPs[i].r, IPs[i].s, IPs[i].t, dN);
-		_jacobian (dN, J);
+		Derivs   (IPs[i].r, IPs[i].s, IPs[i].t, dN);
+		Jacobian (dN, J);
 		vol += det(J);
 	}
 	return vol;
@@ -115,55 +114,53 @@ inline double GeomElem::Volume() const
 inline void GeomElem::Extrap(Vec_t & IPVals, Vec_t & NodVals) const 
 {
 	// Check
-	if (IPVals.Size()!=static_cast<int>(NIPs)) throw new Fatal("Element::Extrap: IPVals.Size()==%d must be equal to NIPs==%d",IPVals.Size(),NIPs);
+	if (IPVals.Size()!=static_cast<int>(NIPs)) throw new Fatal("GeomElem::Extrap: IPVals.Size()==%d must be equal to NIPs==%d",IPVals.Size(),NIPs);
 
 	// Data
 	size_t m = IPVals.Size();
 	size_t n = NNodes;
 	NodVals.Resize(n);
 
-	/* N: shape functions matrix:
-	          _                                                 _
-	         |   N11      N12      N13      ...  N1(nNode)       |
-	         |   N21      N22      N23      ...  N2(nNode)       |
-	     N = |   N31      N32      N33      ...  N3(nNode)       |
-	         |          ...............................          |
-	         |_  N(nIP)1  N(nIP)2  N(nIP)3  ...  N(nIP)(nNode)  _| [m=nIP x n=nNode]
+	/* Nmat: shape functions matrix:
+	             _                                                 _
+	            |   N11      N12      N13      ...  N1(nNode)       |
+	            |   N21      N22      N23      ...  N2(nNode)       |
+	     Nmat = |   N31      N32      N33      ...  N3(nNode)       |
+	            |          ...............................          |
+	            |_  N(nIP)1  N(nIP)2  N(nIP)3  ...  N(nIP)(nNode)  _| [m=nIP x n=nNode]
 	*/
-	Mat_t N(m, n);
-	Vec_t shape(n);
-	Mat_t ip_coord(m, NDim+1);   // IP coordinates matrix (local)
-	Mat_t node_coord;             // nodal coordinates matrix (local)
 
-	// Mount N and ip_coord matrices
+	// Mount Nmat and ip_coords matrices
+	Mat_t Nmat      (m, n);
+	Vec_t N         (n);
+	Mat_t ip_coords (m, NDim+1); // IP coordinates matrix (local)
 	for (size_t i=0; i<m; i++)
 	{
-		Shape (IPs[i].r, IPs[i].s, IPs[i].t, shape);
-		for (size_t j=0; j<n; j++) N(i,j) = shape(j);
-		ip_coord(i, 0) = IPs[i].r;
-		ip_coord(i, 1) = IPs[i].s;
-		if (NDim==2) ip_coord(i, 2) = 1.0;
+		Shape (IPs[i].r, IPs[i].s, IPs[i].t, N);
+		for (size_t j=0; j<n; j++) Nmat(i,j) = N(j);
+		ip_coords(i, 0) = IPs[i].r;
+		ip_coords(i, 1) = IPs[i].s;
+		if (NDim==2) ip_coords(i, 2) = 1.0;
 		else
 		{
-			ip_coord(i, 2) = IPs[i].t;
-			ip_coord(i, 3) = 1.0;
+			ip_coords(i, 2) = IPs[i].t;
+			ip_coords(i, 3) = 1.0;
 		}
 	}
 
-	// Mount node_coord matrix
-	//_local_coords (node_coord);
+	// Nodal coordinates matrix (local)
+	Mat_t coords;
+	_local_coords (coords);
 
-	// Extrapolator matrix
+	// Extrapolation matrix
 	Mat_t E(n, m);
-
-	//calculate extrapolator matrix
-	if (n==m)     E = inv(N);
-	else if (m>n) E = invg(N);
+	     if (n==m) E = inv(Nmat);
+	else if (m>n)  E = invg(Nmat);
 	else
 	{
 		Mat_t I;
 		identity (m,I);
-		E = invg(N)* (I - ip_coord*invg(ip_coord)) + node_coord*invg(ip_coord);
+		E = invg(Nmat)* (I-ip_coords*invg(ip_coords)) + coords*invg(ip_coords);
 	}
 	
 	// Extrapolate
@@ -172,53 +169,50 @@ inline void GeomElem::Extrap(Vec_t & IPVals, Vec_t & NodVals) const
 
 inline void GeomElem::InvMap(double x, double y, double z, double & r, double & s, double & t) const
 {
-	Vec_t shape;
-	Mat_t derivs;
-	Mat_t J; //Jacobian matrix
+	Vec_t N;  // Shape
+	Mat_t dN; // Derivs
+	Mat_t J;  //Jacobian matrix
 	Vec_t f;
 	Vec_t delta;
 	     if (NDim==2) f.Resize(2);
 	else if (NDim==3) f.Resize(3);
 	double tx, ty, tz; //x, y, z trial
 	double norm_f;
-	r = s = t =0; // first suposition for natural coordinates
-	int max_steps= 25;
-	int k=0;
+	r = s = t = 0; // first suposition for natural coordinates
+	int max_steps = 25;
+	int k = 0;
 	for (k=0; k<max_steps; k++)
 	{
-		Shape (r, s, t, shape);
-		Derivs(r, s, t, derivs);
-		Jacobian(derivs, J);
+		Shape    (r, s, t, N);
+		Derivs   (r, s, t, dN);
+		Jacobian (dN, J);
 		tx = ty = tz = 0; 
 
-		//calculate trial of real coordinates
+		// calculate trial of real coordinates
 		for (size_t j=0; j<NNodes; j++) 
 		{
-			tx += shape(j)*Conn[j]->Coord(0); //ok
-			ty += shape(j)*Conn[j]->Coord(1); //ok
-			if (NDim==3)
-			tz += shape(j)*Conn[j]->Coord(2); //ok
+			             tx += N(j)*Conn[j]->Coord(0); // ok
+			             ty += N(j)*Conn[j]->Coord(1); // ok
+			if (NDim==3) tz += N(j)*Conn[j]->Coord(2); // ok
 		}
 		
-		// Calculate the error
-		f(0) = tx - x;
-		f(1) = ty - y;
-		if (NDim==3)
-		f(2) = tz - z;
+		// Calculate error
+		             f(0) = tx - x;
+		             f(1) = ty - y;
+		if (NDim==3) f(2) = tz - z;
 		
-		// Calculate the corrector
+		// Calculate corrector
 		delta = trn(inv(J))*f;
 		
-		r -= delta(0);
-		s -= delta(1);
-		if (NDim==3)
-		t -= delta(2);
+		             r -= delta(0);
+		             s -= delta(1);
+		if (NDim==3) t -= delta(2);
 
 		norm_f = sqrt(trn(f)*f);
 
-		if (norm_f<1.0E4) break;
-	} 
-	if (k==max_steps) throw new Fatal("Element::InverseMap: InverseMap did not converge after %d steps in element", max_steps);
+		if (norm_f<1.0e+4) break;
+	}
+	if (k==max_steps) throw new Fatal("GeomElem::InvMap: InvMap did not converge after %d steps", max_steps);
 }
 
 inline bool GeomElem::IsInside(double X, double Y, double Z) const
@@ -251,14 +245,12 @@ inline bool GeomElem::IsInside(double X, double Y, double Z) const
 	if (Z<min) return false;
 
 	double r, s, t;
-	InverseMap (X,Y,Z, r,s,t);
+	InvMap (X,Y,Z, r,s,t);
 	if (BoundDist(r,s,t)>-tiny) return true;
 	else return false;
 }
 
-/* private */
-
-inline void GeomElem::_jacobian(Mat_t const & dN, Mat_t & J) const
+inline void GeomElem::Jacobian(Mat_t const & dN, Mat_t & J) const
 {
 	Mat_t coords(NNodes, NDim);
 	for (size_t i=0; i<NNodes; ++i)
