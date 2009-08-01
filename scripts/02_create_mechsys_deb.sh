f77de9ea76d6740a98fde3c7c3277bc04b6d275e
Dorival Pedroso <dorival.pedroso@gmail.com>
2009-08-01 22:53:31 +1000

    Bash scripts moved to scripts/ directory

--- scripts/02_create_mechsys_deb.sh
+++ scripts/02_create_mechsys_deb.sh
@@ -10,28 +10,30 @@
 #   sudo pbuilder update --override-config
 #   gpg --gen-key
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
 	echo
 	echo "Usage:"
-	echo "        [1;34msh $0[0m [1;31mVERSION REV[0m"
+	echo "        $0  ARCH"
 	echo
-	echo " where [1;31mVERSION=1.0[0m and [1;31mREV=1[0m, for example"
+	echo "Where:"
+	echo "	  ARCH =  i386  or  amd64"
 	echo
 	exit 1
 fi
 
-VERSION=$1
-REV=$2
+VERSION=1.3
+REV=1
+ARCH=$1
 
 set -e
 
 echo
 echo "[1;34m########################################## Creating source directory[0m"
 echo
-rm -rf mechsys-$VERSION
+rm -rf ./mechsys-$VERSION
 cp -r ~/mechsys/ ./mechsys-$VERSION/
 cd mechsys-$VERSION
-sh Reset_cmake.sh
+sh scripts/Reset_cmake.sh
 rm -rf .hg/
 rm .hgignore
 find . -iname "*.swp" -exec rm {} \;
@@ -63,30 +65,5 @@ echo "[1;34m############################################# Cleaning up temp file
 echo
 rm -f *.build
 rm -rf ./mechsys-$VERSION/
-
-if [ -f /var/cache/pbuilder/result/mechsys_"$VERSION-$REV"_i386.deb ]; then
-	echo
-	echo "[1;34m############################################## Setting up repository[0m"
-	test -d binary || mkdir binary
-	test -d sources || mkdir sources
-	cp /var/cache/pbuilder/result/mechsys_"$VERSION-$REV"_i386.deb binary/
-	mv mechsys_$VERSION-$REV.dsc sources/
-	mv mechsys_$VERSION-$REV.diff.gz sources/
-	mv mechsys_$VERSION.orig.tar.gz sources/
-	mv mechsys_"$VERSION-$REV"_source.changes sources/
-	# binary
-	dpkg-scanpackages binary /dev/null \
-	 | sed 's@Filename: binary/@Filename: /software/mechsys/binary/@' \
-	 | gzip -9c > binary/Packages.gz
-	# sources
-	dpkg-scansources sources /dev/null \
-	 | sed 's@Directory: sources@Directory: /software/mechsys/sources@' \
-	 | gzip -9c > sources/Sources.gz
-else
-	echo
-	echo "ERROR: file [1;31m/var/cache/pbuilder/result/mechsys_"$VERSION-$REV"_i386.deb[0m was not found"
-	echo
-	exit 1
-fi
 
 exit 0
---
+++ scripts/03_gen_repository.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo
+	echo "Usage:"
+	echo "        $0  FLAG"
+	echo
+	echo "FLAG:"
+	echo " 0 => only IGraph"
+	echo " 1 => only MechSys"
+	echo
+	exit 1
+fi
+
+test -d binary  || mkdir binary
+test -d sources || mkdir sources
+
+if [ "$1" -eq 0 ]; then
+	echo "[1;34m############################################## IGraph ####[0m"
+	cp /var/cache/pbuilder/result/libigraph*.deb binary/
+	mv igraph_*.diff.gz     sources/
+	mv igraph_*.dsc         sources/
+	mv igraph_*.changes     sources/
+	mv igraph_*.orig.tar.gz sources/
+fi
+
+if [ "$1" -eq 1 ]; then
+	echo "[1;34m############################################## MechSys ####[0m"
+	cp /var/cache/pbuilder/result/mechsys*.deb binary/
+	mv mechsys_*.diff.gz     sources/
+	mv mechsys_*.dsc         sources/
+	mv mechsys_*.changes     sources/
+	mv mechsys_*.orig.tar.gz sources/
+fi
+
+# binary
+dpkg-scanpackages binary /dev/null \
+ | sed 's@Filename: binary/@Filename: /software/mechsys/binary/@' \
+ | gzip -9c > binary/Packages.gz
+
+# sources
+dpkg-scansources sources /dev/null \
+ | sed 's@Directory: sources@Directory: /software/mechsys/sources@' \
+ | gzip -9c > sources/Sources.gz
+
+exit 0
