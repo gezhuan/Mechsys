7b546229fb19cd19fc0facd2765f535d45e88525
Dorival Pedroso <dorival.pedroso@gmail.com>
2009-10-23 16:04:33 +1000

    scripts to help patch added (corrected)

--- patches/tetgen/do_patch.sh
+++ patches/tetgen/do_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-patch makefile         $HOME/mechsys/patches/tetgen/makefile.diff
-patch tetgen1.4.3.cxx  $HOME/mechsys/patches/tetgen/tetgen1.4.3.cxx.diff
-patch tetgen1.4.3.h    $HOME/mechsys/patches/tetgen/tetgen1.4.3.h.diff
+patch makefile    $HOME/mechsys/patches/tetgen/makefile.diff
+patch tetgen.cxx  $HOME/mechsys/patches/tetgen/tetgen1.4.3.cxx.diff
+patch tetgen.h    $HOME/mechsys/patches/tetgen/tetgen1.4.3.h.diff
