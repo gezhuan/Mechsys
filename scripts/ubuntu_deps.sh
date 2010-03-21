c0854840be24b25fe52f70f7e259aeeafccd644e
Dorival Pedroso <dorival.pedroso@gmail.com>
2010-03-21 10:50:56 +1000

    Scripts: installation of dependencies updated

--- scripts/ubuntu_deps.sh
+++ scripts/ubuntu_deps.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-sudo apt-get install g++ cmake gfortran libblitz0-dev libboost-python1.40-dev \
-                     libgsl0-dev mercurial subversion wget
+sudo apt-get install g++ wget cmake patch gfortran mercurial subversion libgsl0-dev libblitz0-dev \
+                     cmake-curses-gui libsuitesparse-dev libboost-python1.40-dev \
+                     python-tk python-numpy python-scipy python-matplotlib
