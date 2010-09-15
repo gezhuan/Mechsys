7fea2b4ea7e770bc3f470579d459897272f868b5
Dorival Pedroso <dorival.pedroso@gmail.com>
2010-09-15 16:09:02 +1000

    Numerical: Runge-Kuta2==Modified-Euler added

---
+++ test/plot_ode.py
@@ -0,0 +1,25 @@
+from numpy import exp, log, linspace
+from pylab import plot, show, xlabel, ylabel, grid, legend, axis, subplot
+from msys_readdata import *
+
+fsz = {'fontsize':20}
+dat = read_table("test_ode1.dat")
+
+subplot(2,1,1)
+plot   (dat['t'], dat['y0'],   'b-', label='GSL')
+plot   (dat['t'], dat['y0me'], 'r.', label='ME')
+xlabel (r'$t$',  fsz)
+ylabel (r'$y_0$',fsz)
+legend ()
+grid   ()
+
+subplot(2,1,2)
+plot   (dat['y0'],   dat['y1'],   'b-', label='GSL')
+plot   (dat['y0me'], dat['y1me'], 'r.', label='ME')
+xlabel (r'$y_0$',  fsz)
+ylabel (r'$y_1$',fsz)
+legend ()
+#axis   ('scaled')
+grid   ()
+
+show   ()
