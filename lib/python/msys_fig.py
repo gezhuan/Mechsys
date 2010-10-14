4486f3ba50506653c1d63de91afe9abcc7688698
Dorival Pedroso <dorival.pedroso@gmail.com>
2010-10-14 19:00:06 +1000

    Python: script to set EPS added

---
+++ lib/python/msys_fig.py
@@ -0,0 +1,19 @@
+from pylab import rcParams
+
+def set_eps (proport=0.75):
+    fig_width_pt  = 455.24411                   # Get this from LaTeX using \showthe\columnwidth
+    inches_per_pt = 1.0/72.27                   # Convert pt to inch
+    fig_width     = fig_width_pt*inches_per_pt  # width in inches
+    fig_height    = fig_width*proport           # height in inches
+    fig_size      = [fig_width,fig_height]
+
+    params = {'backend':         'ps',
+              'axes.labelsize':  10,
+              'text.fontsize':   10,
+              'legend.fontsize': 9,
+              'xtick.labelsize': 8,
+              'ytick.labelsize': 8,
+              'text.usetex':     False,
+              'figure.figsize': fig_size}
+
+    rcParams.update(params)
