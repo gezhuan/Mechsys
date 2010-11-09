c04ad340ff121bfc58b3216aa8151bbe4e216c1c
Dorival Pedroso <dorival.pedroso@gmail.com>
2010-11-10 00:13:03 +1000

    FEM: HydroMech improved (GN22 is converging :-)

--- lib/fem/hydromechelem.h
+++ lib/fem/hydromechelem.h
@@ -32,6 +32,8 @@ public:
     // Static
     static size_t NDp; ///< Number of DOFs of pressure = GEp->NN
     static size_t NDt; ///< Total number of DOFs = NDu + NDp
+    static Mat_t  Im;  ///< Identity column matrix (NCo,1)
+    static Vec_t  Iv;  ///< Identity vector (NCo)
 
     // Constructor
     HydroMechElem (int                  NDim,   ///< Space dimension
@@ -41,19 +43,25 @@ public:
                    SDPair       const & Ini,    ///< Initial values
                    Array<Node*> const & Nodes); ///< Connectivity
 
-    // Destructor
-    ~HydroMechElem () { if (GEp!=NULL) delete GEp; }
-
     // Methods
-    void CalcKCM (Mat_t & KK, Mat_t & CC, Mat_t & MM) const;
-    void GetLoc  (Array<size_t> & Loc)                const; ///< Get location vector for mounting KCM matrices
+    void CalcKCM     (Mat_t & KK, Mat_t & CC, Mat_t & MM)   const;
+    void GetLoc      (Array<size_t> & Loc)                  const;
+    void UpdateState (Vec_t const & dU, Vec_t * F_int=NULL) const;
+
+    // Internal Methods
+    void Interp (Mat_t const & C, IntegPoint const & IP, Mat_t & B, Mat_t & Bp, Mat_t & N, Mat_t & Np, double & detJ, double & Coef) const; ///< Interpolation matrices
 
     // Data
-    GeomElem * GEp; ///< Element for the pressure DOFs
+    double cval;
+    double Cval;
+    double chi;
+    Mat_t  kb; // == k/gammaW
 };
 
 size_t HydroMechElem::NDp = 0;
 size_t HydroMechElem::NDt = 0;
+Mat_t  HydroMechElem::Im;
+Vec_t  HydroMechElem::Iv;
 
 
 /////////////////////////////////////////////////////////////////////////////////////////// Implementation /////
@@ -62,15 +70,18 @@ size_t HydroMechElem::NDt = 0;
 inline HydroMechElem::HydroMechElem (int NDim, Mesh::Cell const & Cell, Model const * Mdl, SDPair const & Prp, SDPair const & Ini, Array<Node*> const & Nodes)
     : EquilibElem(NDim,Cell,Mdl,Prp,Ini,Nodes)
 {
-    // allocate GEp
-    if (GE->Name=="Hex20") GEp = AllocGeomElem ("Hex8", NDim);
-    else throw new Fatal("HydroMechElem::HydroMechElem: Geom element==%s is not implemented yet",GE->Name.CStr());
-
     // set constants of this class (just once)
     if (NDp==0)
     {
-        NDp = GEp->NN;
+        NDp = GE->NN;
         NDt = NDu + NDp;
+
+        Im.change_dim (NCo,1);
+        Iv.change_dim (NCo);
+        set_to_zero (Im);
+        set_to_zero (Iv);
+        Im(0,0)=1.0;   Im(1,0)=1.0;   Im(2,0)=1.0;
+        Iv(0)  =1.0;   Iv(1)  =1.0;   Iv(2)  =1.0;
     }
 
     // set initial pw values at Nodes
@@ -82,7 +93,7 @@ inline HydroMechElem::HydroMechElem (int NDim, Mesh::Cell const & Cell, Model co
         bool   pos_pw  = (Ini.HasKey("only_positive_pw") ? true : false);
         double gamW    = Ini("gamW");
         double z_water = (NDim==2 ? Ini("y_water") : Ini("z_water"));
-        for (size_t i=0; i<GEp->NN; ++i)
+        for (size_t i=0; i<GE->NN; ++i)
         {
             // elevation of node
             double z = (NDim==2 ? Con[i]->Vert.C(1) : Con[i]->Vert.C(2));
@@ -95,39 +106,117 @@ inline HydroMechElem::HydroMechElem (int NDim, Mesh::Cell const & Cell, Model co
             Con[i]->U("pw") = pw;
         }
     }
+
+    // hydraulic conductivity tensor
+    kb.change_dim(NDim,NDim);
+    set_to_zero (kb);
+    kb(0,0)=1;  kb(1,1)=1;  if (NDim==3) kb(2,2)=1;
+    double gammaW = 1.;
+    kb  /= gammaW;
+    cval = 1.0;
+    Cval = 0.0;
+    chi  = 1.0;
 }
 
 inline void HydroMechElem::GetLoc (Array<size_t> & Loc) const
 {
-    // u DOFs
     Loc.Resize (NDt);
     for (size_t i=0; i<GE->NN; ++i)
     {
         Loc[i*NDim+0] = Con[i]->Eq("ux");
         Loc[i*NDim+1] = Con[i]->Eq("uy");  if (NDim==3)
         Loc[i*NDim+2] = Con[i]->Eq("uz");
+        Loc[NDu+i]    = Con[i]->Eq("pw");
+    }
+}
+
+inline void HydroMechElem::Interp (Mat_t const & C, IntegPoint const & IP, Mat_t & B, Mat_t & Bp, Mat_t & N, Mat_t & Np, double & detJ, double & Coef) const
+{
+    // deriv of shape func w.r.t natural coordinates
+    GE->Shape  (IP.r, IP.s, IP.t);
+    GE->Derivs (IP.r, IP.s, IP.t);
+
+    // Jacobian and its determinant
+    Mat_t J(GE->dNdR * C); // J = dNdR * C
+    detJ = Det(J);
+
+    // deriv of shape func w.r.t real coordinates
+    Mat_t Ji;
+    Inv (J, Ji);
+    Mat_t dNdX(Ji * GE->dNdR); // dNdX = Inv(J) * dNdR
+
+    // coefficient used during integration
+    Coef = h*detJ*IP.w;
+
+    // B matrix
+    B.change_dim (NCo,NDu);
+    set_to_zero  (B);
+    if (NDim==2)
+    {
+        if (GTy==axs_t)
+        {
+            // correct Coef
+            double radius = 0.0; // radius=x at this IP
+            for (size_t j=0; j<GE->NN; ++j) radius += GE->N(j)*Con[j]->Vert.C[0];
+            Coef *= radius;
+
+            // B matrix
+            for (size_t i=0; i<GE->NN; ++i)
+            {
+                B(0,0+i*NDim) = dNdX(0,i);
+                B(1,1+i*NDim) = dNdX(1,i);
+                B(2,0+i*NDim) = GE->N(i)/radius;
+                B(3,0+i*NDim) = dNdX(1,i)/sqrt(2.0);
+                B(3,1+i*NDim) = dNdX(0,i)/sqrt(2.0);
+            }
+        }
+        else // pse_t, psa_t
+        {
+            for (size_t i=0; i<GE->NN; ++i)
+            {
+                B(0,0+i*NDim) = dNdX(0,i);
+                B(1,1+i*NDim) = dNdX(1,i);
+                B(3,0+i*NDim) = dNdX(1,i)/sqrt(2.0);
+                B(3,1+i*NDim) = dNdX(0,i)/sqrt(2.0);
+            }
+        }
+    }
+    else // 3D
+    {
+        for (size_t i=0; i<GE->NN; ++i)
+        {
+            B(0,0+i*NDim) = dNdX(0,i);
+            B(1,1+i*NDim) = dNdX(1,i);
+            B(2,2+i*NDim) = dNdX(2,i);
+            B(3,0+i*NDim) = dNdX(1,i)/sqrt(2.0);   B(3,1+i*NDim) = dNdX(0,i)/sqrt(2.0);
+            B(4,1+i*NDim) = dNdX(2,i)/sqrt(2.0);   B(4,2+i*NDim) = dNdX(1,i)/sqrt(2.0);
+            B(5,2+i*NDim) = dNdX(0,i)/sqrt(2.0);   B(5,0+i*NDim) = dNdX(2,i)/sqrt(2.0);
+        }
     }
 
-    // pw DOFs
-    for (size_t i=0; i<GEp->NN; ++i)
-    {
-        Loc[NDu+i] = Con[i]->Eq("pw");
-    }
+    // Bp matrix
+    Bp = dNdX;
+
+    // N matrix
+    N.change_dim (NDim,NDu);
+    set_to_zero  (N);
+    for (int    i=0; i<NDim;   ++i)
+    for (size_t j=0; j<GE->NN; ++j)
+        N(i,i+j*NDim) = GE->N(j);
+
+    // Np matrix
+    Np.change_dim (1,NDp);
+    for (size_t j=0; j<GE->NN; ++j) Np(0,j) = GE->N(j);
 }
 
 inline void HydroMechElem::CalcKCM (Mat_t & KK, Mat_t & CC, Mat_t & MM) const
 {
-    // hydraulic conductivity tensor
-    Mat_t kb(NDim,NDim);
-    set_to_zero (kb);
-    kb(0,0)=1;  kb(1,1)=1;  if (NDim==3) kb(2,2)=1;
-    double gammaW = 1.;
-    kb /= gammaW;
-
     // mechanical matrices
-    Mat_t M, K;
-    EquilibElem::CalcM (M);
-    EquilibElem::CalcK (K);
+    Mat_t M, K, D;
+    M.change_dim (NDu,NDu); // mass matrix
+    K.change_dim (NDu,NDu); // stiffness matrix
+    set_to_zero (M);
+    set_to_zero (K);
 
     // hydraulic matrices
     Mat_t Q, Qb, H, S;
@@ -141,28 +230,29 @@ inline void HydroMechElem::CalcKCM (Mat_t & KK, Mat_t & CC, Mat_t & MM) const
     set_to_zero (S);
 
     // auxiliar matrices
-    Mat_t C;
+    Mat_t C, B, Bp, N, Np;
     double detJ, coef;
-    //Mat_t NtN    (NDu,NDu);
-    //Mat_t BtDB   (NDu,NDu);
-    //Mat_t BtINp  (NDu,NDp);
-    //Mat_t BptkBp (NDp,NDp);
-    //Mat_t NptNp  (NDp,NDp);
+    Mat_t NtN    (NDu,NDu);
+    Mat_t BtDB   (NDu,NDu);
+    Mat_t BtmNp  (NDu,NDp);
+    Mat_t NptNp  (NDp,NDp);
+    Mat_t BptkBp (NDp,NDp);
     CoordMatrix (C);
     for (size_t i=0; i<GE->NIP; ++i)
     {
-        //Mdl->Stiffness (Sta[i], D);
-        //Interp (C, GE->IPs[i], B, Bp, N, Np, detJ, coef);
-        //NtN    = trans(N)*N;
-        //BtDB   = trans(B)*D*B;
-        //BtINp  = trans(B)*Im*Np;
-        //BptkBp = trans(Bp)*km*Bp;
-        //NptNp  = trans(Np)*Np;
-        //M     += (coef*rho) * NtN;
-        //K     += (coef)     * BtDB;
-        //L     += (coef*alp) * BtINp;
-        //H     += (coef)     * BptkBp;
-        //S     += (coef/Qs)  * NptNp;
+        Mdl->Stiffness (Sta[i], D);
+        Interp (C, GE->IPs[i], B, Bp, N, Np, detJ, coef);
+        NtN    = trans(N)*N;
+        BtDB   = trans(B)*D*B;
+        BtmNp  = trans(B)*Im*Np;
+        NptNp  = trans(Np)*Np;
+        BptkBp = trans(Bp)*kb*Bp;
+        M     += (coef*rho)  * NtN;
+        K     += (coef)      * BtDB;
+        Q     += (coef*cval) * BtmNp;
+        Qb    += (coef*chi)  * BtmNp;
+        S     += (coef*Cval) * NptNp;
+        H     += (coef)      * BptkBp;
     }
 
     // assemble
@@ -193,6 +283,60 @@ inline void HydroMechElem::CalcKCM (Mat_t & KK, Mat_t & CC, Mat_t & MM) const
     }
 }
 
+inline void HydroMechElem::UpdateState (Vec_t const & dU, Vec_t * F_int) const
+{
+    // get location array
+    Array<size_t> loc;
+    GetLoc (loc);
+
+    // element nodal displacements
+    Vec_t dUe(NDu);
+    for (size_t i=0; i<NDu; ++i) dUe(i) = dU(loc[i]);
+
+    // element nodal pore-water pressure
+    Vec_t dpwe(NDp);
+    for (size_t i=0; i<NDp; ++i) dpwe(i) = dU(loc[NDu+i]);
+
+    // auxiliary matrices and vectors
+    Mat_t kBp     (NDim, NDp);
+    Vec_t kBpdpwe (NDim);
+
+    // update state at each IP
+    StressUpdate su(Mdl);
+    double detJ, coef;
+    Mat_t  C, B, Bp, N, Np;
+    Vec_t  dFe(NDu), dsig(NCo), deps(NCo), dfe(NDp);
+    set_to_zero (dFe);
+    set_to_zero (dfe);
+    CoordMatrix (C);
+    for (size_t i=0; i<GE->NIP; ++i)
+    {
+        // B matrix
+        Interp (C, GE->IPs[i], B, Bp, N, Np, detJ, coef);
+
+        // strain and effective stress increments
+        deps = B * dUe;
+        su.Update (deps, Sta[i], dsig);
+
+        // total stress increments
+        Vec_t Npdpwe(Np * dpwe);
+        double dpw = Npdpwe(0);
+        dsig -= chi*dpw*Iv;
+
+        // element nodal forces
+        kBp     = kb * Bp;
+        kBpdpwe = kBp * dpwe;
+        dFe += (coef) * trans(B) *dsig;
+        dfe += (coef) * trans(Bp)*kBpdpwe;
+    }
+
+    // add results to Fint (internal forces)
+    if (F_int!=NULL)
+    {
+        for (size_t i=0; i<NDu; ++i) (*F_int)(loc[i])     += dFe(i);
+        for (size_t i=0; i<NDp; ++i) (*F_int)(loc[NDu+i]) += dfe(i);
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////// Factory /////
 
@@ -205,7 +349,6 @@ int HydroMechElemRegister()
 {
     ElementFactory["HydroMech"]   = HydroMechElemMaker;
     ElementVarKeys["HydroMech2D"] = std::make_pair ("ux uy pw",    "fx fy qw");
-    //ElementVarKeys["HydroMech3D"] = std::make_pair ("ux uy uz", "fx fy fz");
     ElementVarKeys["HydroMech3D"] = std::make_pair ("ux uy uz pw", "fx fy fz qw");
     PROB.Set ("HydroMech", (double)PROB.Keys.Size());
     return 0;
